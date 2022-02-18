d9ea66d5a604a39b171e59485019a2a3c15c1ef9
Z. Simpson <zsimpson@unbc.ca>
2022-02-18 15:15:51 -0800

    create Die class

---
+++ src/equipment/Die.java
@@ -0,0 +1,16 @@
+package equipment;
+
+import java.util.Random;
+
+public class Die {
+    private Random rand;
+
+    public Die(){
+        rand = new Random();
+    }
+
+    public int roll(){
+        return rand.nextInt(6) + 1;
+    }
+
+}
