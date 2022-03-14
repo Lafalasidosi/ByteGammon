cd3f737d8bbddf8ed887eb6a26920c1726db7047
GitHMeister <93025605+GitHMeister@users.noreply.github.com>
2022-03-14 15:26:05 -0700

    Committing

---
+++ src/gui/GUI2.java
@@ -0,0 +1,14 @@
+package gui;
+
+import equipment.Board;
+import javax.swing.*;
+import equipment.Checker;
+
+public class GUI2 javax.swing.JFrame {
+
+
+    public static void SetUpBoard (Board sBoard)
+    {
+
+    }
+}
