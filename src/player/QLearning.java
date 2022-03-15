acbd97762fe89886bb792e02725b1b831d04a3fd
Noah <nelliotbrown@gmail.com>
2022-03-14 18:59:35 -0700

    Added the number of states to the q-learning algo

--- src/player/QLearning.java
+++ src/player/QLearning.java
@@ -1,5 +1,6 @@
 package player;
 
+import equipment.Board;
 import whoiswho.Colour;
 import world.World;
 
@@ -10,9 +11,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class QLearning {
-    World w = new World(Config.TRUE);
-    Ai theBot = new Ai(Colour.BLACK, w);
-    int boardSize;
     private final double alpha = 0.1; //the learning rate
     private final double gamma =  0.9;
     /* "eagerness"; closer to 0 = more short sighted, faster learning value
@@ -20,6 +18,7 @@ public class QLearning {
     */
     private final int reward = 10;
     private final int penalty = -1;
-    private final int statecount;
+    private final int statecount = 24;
+    //provides the number of states equivalent to points on the board
 
 }
