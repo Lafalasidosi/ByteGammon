8b572d55187fc78cfaadbc46b1d873f0bb739e98
Noah <nelliotbrown@gmail.com>
2022-03-14 17:18:16 -0700

    Began work on the proper ai

--- src/player/QLearning.java
+++ src/player/QLearning.java
@@ -1,4 +1,25 @@
 package player;
 
+import whoiswho.Colour;
+import world.World;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class QLearning {
+    World w = new World(Config.TRUE);
+    Ai theBot = new Ai(Colour.BLACK, w);
+    int boardSize;
+    private final double alpha = 0.1; //the learning rate
+    private final double gamma =  0.9;
+    /* "eagerness"; closer to 0 = more short sighted, faster learning value
+    closer to 1 = more far sighted, longer learning value
+    */
+    private final int reward = 10;
+    private final int penalty = -1;
+    private final int statecount;
+
 }
